de142577d0350427733cae19270f1a244429ab6d
Yuntachy <marianarock@gmail.com>
2022-07-13 23:06:46 -0300

    Fist component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,14 @@
-
+import NotificationButton from "./components/NotificationButton"
 
 function App() {
- 
+
 
   return (
-    <h1>Ola mundo!</h1>
+    <>
+      <h1>Ola mundo!</h1>
+      <NotificationButton />
+      <NotificationButton />
+    </>
   )
 }
 
---
+++ frontend/src/components/NotificationButton/index.tsx
@@ -0,0 +1,11 @@
+import icon from '../../assets/img/Nitificationicon.svg'
+import './style.css'
+function NotificationButton() {
+    return (
+        <div className="dsmeta-red-btn" >
+            <img src={icon} alt="Notificação"/>
+        </div>
+    )
+}
+
+export default NotificationButton
